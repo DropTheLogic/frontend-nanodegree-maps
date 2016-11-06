562f6f6ffb5bc6255f64fe1708d00ad1b110f51a
Daniel Marquez <DannyBrianMarquez@gmail.com>
2016-11-06 11:10:24 -0500

    feat: Add branding and simple CSS

--- js/app.js
+++ js/app.js
@@ -1,5 +1,32 @@
 var MapsViewModel = function() {
 	var self = this;
+	// Dummy locations
+	self.listings = ko.observableArray([
+		{
+			'name': 'Here',
+			'address': '123 1 Ave'
+		},
+		{
+			'name': 'There',
+			'address': '123 2 Ave'
+		},
+		{
+			'name': 'Everywhere',
+			'address': '123 3 Ave'
+		},
+		{
+			'name': 'Somewhere',
+			'address': '123 4 Ave'
+		},
+		{
+			'name': 'Else',
+			'address': '123 5 Ave'
+		},
+		{
+			'name': 'If',
+			'address': '123 6 Ave'
+		}
+	]);
 };
 
 ko.applyBindings(new MapsViewModel);
