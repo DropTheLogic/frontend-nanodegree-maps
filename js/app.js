e933522d35de7f2e2016edba6f9e0c7f4f9bd91c
Daniel Marquez <DannyBrianMarquez@gmail.com>
2016-11-07 10:16:53 -0500

    feat: Add functionality to filter input
    * Use knockoutjs's `visible` binding on location list to evaluate
      whether or not to show each location, based on user filter inputted.
    
    * isFiltered function check the given listing parameter against the
      current filter input.
    
    * Move locations outside of ViewModel. Load locations into an
      observableArray from within ViewModel.
    
    * Remove dummy address for real locations.
    
    * Increase side panel by 18px to allow for scrollbar. Also
      remove scrollbar when hidden, to maintain styled border look.
    
    * Update styles and placement for filter input.

--- js/app.js
+++ js/app.js
@@ -8,36 +8,92 @@ var initMap = function() {
 	});
 };
 
+var dataListings = [
+	{
+		'name': 'Umami Burger',
+		'address': '158 N 4th St'
+	},
+	{
+		'name': 'Blue Collar',
+		'address': '160 Havemeyer St'
+	},
+	{
+		'name': 'DuMont Burger',
+		'address': '314 Bedford Ave'
+	},
+	{
+		'name': 'Peter Luger Steak House',
+		'address': '178 Broadway'
+	},
+	{
+		'name': 'Diner',
+		'address': '85 Broadway'
+	},
+	{
+		'name': "Pop's Burger",
+		'address': '167 N 8th St'
+	},
+	{
+		'name': 'Ramen Burger',
+		'address': '90 Kent Ave'
+	},
+	{
+		'name': 'The Burger Guru',
+		'address': '98 Berry St'
+	},
+	{
+		'name': 'Allswell',
+		'address': '124 Bedford Ave'
+	},
+	{
+		'name': 'The Commodore',
+		'address': '366 Metropolitan Ave'
+	},
+	{
+		'name': "Walter Foods",
+		'address': '253 Grand St'
+	},
+	{
+		'name': 'Cow & Clover',
+		'address': '291 Kent Ave'
+	},
+	{
+		'name': 'Checkers',
+		'address': '277 Broadway'
+	}
+];
+
 var MapsViewModel = function() {
 	var self = this;
-	// Dummy locations
-	self.listings = ko.observableArray([
-		{
-			'name': 'Here',
-			'address': '123 1 Ave'
-		},
-		{
-			'name': 'There',
-			'address': '123 2 Ave'
-		},
-		{
-			'name': 'Everywhere',
-			'address': '123 3 Ave'
-		},
-		{
-			'name': 'Somewhere',
-			'address': '123 4 Ave'
-		},
-		{
-			'name': 'Else',
-			'address': '123 5 Ave'
-		},
-		{
-			'name': 'If',
-			'address': '123 6 Ave'
+
+	// Load listing data into observable array
+	self.listings = ko.observableArray();
+	dataListings.forEach(function(item) {
+		self.listings.push({name: item.name, address: item.address});
+	});
+
+	self.filter = ko.observable('');
+
+	// Find if a given listing is within input filter
+	self.isFiltered = function(listing) {
+		var name = listing.name.toLowerCase();
+		var address = listing.address.toLowerCase();
+		var filter = self.filter().toLowerCase();
+		if (name.indexOf(filter) !== -1 || address.indexOf(filter) !== -1) {
+			return true;
 		}
-	]);
-	self.filter = ko.observable();
+		return false;
+	};
+
+	// Counts number of results based on filter, outputs string based on count
+	self.filterCountString = ko.computed(function() {
+		var count = 0;
+		self.listings().forEach(function(listing) {
+			if (self.isFiltered(listing))
+				count++;
+		});
+		return '(' + count + ((count === 1) ? ' place' : ' places') + ' found)';
+	}, this);
 };
 
 ko.applyBindings(new MapsViewModel);
