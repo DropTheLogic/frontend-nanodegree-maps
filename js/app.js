0bc0ff6abcac9a2f2ebfde472ee36e89a61879e4
Daniel Marquez <DannyBrianMarquez@gmail.com>
2016-11-09 02:19:38 -0500

    feat: Add location and google place ID data

--- js/app.js
+++ js/app.js
@@ -28,8 +28,9 @@ var initMap = function() {
 				// Push marker into array
 				markers.push(marker);
 				// TODO: Create click event for infoWindow on each marker
+			} else {
+				alert('request failed due to: ' + status);
 			}
-			// TODO: Make error handling
 		});
 	});
 };
@@ -37,55 +38,68 @@ var initMap = function() {
 var dataListings = [
 	{ name: 'Umami Burger',
 	  address: '158 N 4th St, Brooklyn, NY',
+	  location: {lat: 40.71588719999999, lng: -73.9592588},
 	  googleID: 'ChIJESPDHl5ZwokRcj6FbVLbuew'
 	},
 	{ name: 'Blue Collar',
 	  address: '160 Havemeyer St, Brooklyn, NY',
-	  googleID: ''
+	  location: {lat: 40.7114917, lng: -73.9578698},
+	  googleID: 'ChIJqzsHPOBbwokRx_MN2OEJ6BM'
 	},
 	{ name: 'DuMont Burger',
 	  address: '314 Bedford Ave, Brooklyn, NY',
-	  googleID: ''
+	  location: {lat: 40.7136831, lng: -73.9621056},
+	  googleID: 'ChIJ10LYAt9bwokRpENP2An7El8'
 	},
 	{ name: 'Peter Luger Steak House',
 	  address: '178 Broadway, Brooklyn, NY',
-	  googleID: ''
+	  location: {lat: 40.709819, lng: -73.962467},
+	  googleID: 'ChIJR_bK295bwokR8gM6QgEdmkY'
 	},
 	{ name: 'Diner',
 	  address: '85 Broadway, Brooklyn, NY',
-	  googleID: ''
+	  location: {lat: 40.7106886, lng: -73.9655691},
+	  googleID: 'ChIJvzYmKtlbwokRHye6SmfknDM'
 	},
 	{ name: "Pop's Burger",
 	  address: '167 N 8th St, Brooklyn, NY',
-	  googleID: ''
+	  location: {lat: 40.7181769, lng: -73.95702349999999},
+	  googleID: 'ChIJJd87tF1ZwokRztj3_6guix4'
 	},
 	{ name: 'Ramen Burger',
 	  address: '90 Kent Ave, Brooklyn, NY',
-	  googleID: ''
+	  location: {lat: 40.7214853, lng: -73.9621174},
+	  googleID: 'ChIJTV4tpWdZwokRtZZtuDBZjTY'
 	},
 	{ name: 'The Burger Guru',
 	  address: '98 Berry St, Brooklyn, NY',
-	  googleID: ''
+	  location: {lat: 40.71917699999999, lng: -73.958643},
+	  googleID: 'ChIJyYFam11ZwokRWMaL1wvAFlo'
 	},
 	{ name: 'Allswell',
 	  address: '124 Bedford Ave, Brooklyn, NY',
-	  googleID: ''
+	  location: {lat: 40.7196568, lng: -73.9559515},
+	  googleID: 'ChIJa7Bx-1xZwokRLAsf1yepN3Y'
 	},
 	{ name: 'The Commodore',
 	  address: '366 Metropolitan Ave, Brooklyn, NY',
-	  googleID: ''
+	  location: {lat: 40.7139371, lng: -73.9558631},
+	  googleID: 'ChIJpwY0A19ZwokR3XNKVbz8Lqg'
 	},
 	{ name: "Walter Foods",
 	  address: '253 Grand St, Brooklyn, NY',
-	  googleID: ''
+	  location: {lat: 40.713555, lng: -73.958551},
+	  googleID: 'ChIJIa3vw19ZwokR6gAqP0kXg4A'
 	},
 	{ name: 'Cow & Clover',
 	  address: '291 Kent Ave, Brooklyn, NY',
-	  googleID: ''
+	  location: {lat: 40.71452780000001, lng: -73.9665327},
+	  googleID: 'ChIJresRPmJZwokRGNjAHuO7CQw'
 	},
 	{ name: 'Checkers',
 	  address: '277 Broadway, Brooklyn, NY',
-	  googleID: ''
+	  location: {lat: 40.7088007, lng: -73.9586784},
+	  googleID: 'ChIJ-_HAkOBbwokRiKV330CJkBU'
 	}
 ];
 
